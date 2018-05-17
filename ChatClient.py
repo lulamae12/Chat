a90cec3aa96745fa62d999ae677e05058f89951b
lulamae12 <tls.tommy11@gmail.com>
2018-05-17 12:33:23 -0500

    madfe messages send

--- ChatClient.py
+++ ChatClient.py
@@ -1,6 +1,6 @@
 #Clientside chat
 import socket
-
+import time
 clientSocket = socket.socket() #create a new Clientside socket
 clientHostname = socket.gethostname()#get hostname of machine
 port = 12345 #set connection port
@@ -9,5 +9,7 @@ clientSocket.connect((clientHostname,port))
 print(clientSocket.recv(1024))#print recived data from port 1024 where the server is
 while True:
     message = input("->")#get message to send
+    #time.sleep(0.4)
+    clientSocket.sendto(message.encode('utf-8'), (clientHostname, port))# encrypt message  and send
     clientSocket.sendto(message.encode('utf-8'), (clientHostname, port))# encrypt message  and send
 clientSocket.close # close when done
--- ChatServer.py
+++ ChatServer.py
@@ -1,16 +1,18 @@
 
 #server side chat
 import socket
-
 serverSocket = socket.socket()#create new serverside socket
 serverHostname = socket.gethostname()#get hostname of server
 port = 12345 #set connection port
 serverSocket.bind((serverHostname, port))#bind to port
+ConnectionMessage = ("You have connected to:" + serverHostname)
 serverSocket.listen(5)
 client, address = serverSocket.accept()#establish connection with client
 print("Connected to:", address)
-#client.send("You have conneted to the server.")
+message = ConnectionMessage
+client.sendto(message.encode(),(serverHostname, port))
 while True:
-        data = serverSocket.recv(1024)
-        modifiedMessage, serverAddress = clientSocket.recvfrom(message.decode('utf-8'))
+        data = client.recv(1024)
+        #modifiedMessage, serverAddress = serverSocket.recvfrom(message.decode('utf-8'))
+        print('Received message:', client.recv(1024))
 client.close()
