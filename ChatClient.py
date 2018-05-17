1df01da3ffa15e1ff8eabada06e386edfe731e87
lulamae12 <tls.tommy11@gmail.com>
2018-05-17 12:43:52 -0500

    added stuff

--- ChatClient.py
+++ ChatClient.py
@@ -8,6 +8,7 @@ port = 12345 #set connection port
 clientSocket.connect((clientHostname,port))
 print(clientSocket.recv(1024))#print recived data from port 1024 where the server is
 while True:
+
     message = input("->")#get message to send
     #time.sleep(0.4)
     clientSocket.sendto(message.encode('utf-8'), (clientHostname, port))# encrypt message  and send
--- ChatServer.py
+++ ChatServer.py
@@ -5,14 +5,15 @@ serverSocket = socket.socket()#create new serverside socket
 serverHostname = socket.gethostname()#get hostname of server
 port = 12345 #set connection port
 serverSocket.bind((serverHostname, port))#bind to port
-ConnectionMessage = ("You have connected to:" + serverHostname)
+ConnectionMessage = (serverHostname + "has Connected.")
 serverSocket.listen(5)
 client, address = serverSocket.accept()#establish connection with client
 print("Connected to:", address)
 message = ConnectionMessage
-client.sendto(message.encode(),(serverHostname, port))
+client.sendto(message.encode(),(serverHostname, port))#send message to client
 while True:
         data = client.recv(1024)
         #modifiedMessage, serverAddress = serverSocket.recvfrom(message.decode('utf-8'))
         print('Received message:', client.recv(1024))
+
 client.close()
